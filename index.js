306ba1c3484b7bb45aa56f5ccef64b9195c27c22
Keisi Polli <keisi.polli@khk.ee>
2022-03-29 17:21:36 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-// add template engine
+
+
 const hbs = require('express-handlebars')
-// setup template engine directory and files extensions
+
+
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 app.engine('hbs', hbs.engine({
@@ -13,12 +16,15 @@ app.engine('hbs', hbs.engine({
     layoutsDir: __dirname + '/views/layouts',
 }))
 
-// setup static public directory
+
+
 app.use(express.static('public'))
 
 const mysql = require('mysql')
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
+
 
 
 var con = mysql.createConnection({
@@ -27,12 +33,26 @@ var con = mysql.createConnection({
     password: 'qwerty',
     database: 'joga_mysql'
 })
-
 con.connect(function (err) {
     if (err) throw err;
-    console.log('Connected to joga_mysql db');
+    console.log('Connected to joga_mysql database');
 })
+
+app.get('/', (req, res) => {
+    let query = "SELECT * FROM article";
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err;
+        articles = result
+        res.render('index', {
+        articles: articles
+    })
+    })
+});
+
+
+
 
 app.listen(3000, () => {
     console.log('App is started at http://localhost:3000')
-})
+});
